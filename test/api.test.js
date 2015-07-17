54fbe47ccbe2682dd84bc45fce810ede304a2d4b
GP <exchequer598@gmail.com>
2015-07-17 09:58:32 +0530

    Update testcases, add new ones for Forbidden, and cleanup/lint code.

--- test/api.test.js
+++ test/api.test.js
@@ -1,172 +1,227 @@
-var request = require('supertest')
-  , expect = require('expect.js')
-  , express = require('express')
-  , api = require('../');
+'use strict';
+
+var request = require('supertest'),
+    expect = require('expect.js'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    api = require('../');
 
 process.env.NODE_ENV = 'test';
 
-describe('the api', function () {
-  it('should respond with 200 status for valid requests', function (done) {
-    var app = express()
-      , data = { message: 'ok' };
+describe('the api', function() {
+    it('should respond with 200 status for valid requests', function(done) {
+        var app = express(),
+            data = {
+                message: 'ok'
+            };
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        app.post('/api/test/:username', function(req, res) {
+            api.requireParams(req, res, ['username', 'password', 'token'], function(err) {
+                api.ok(req, res, {
+                    message: 'ok'
+                });
+            });
+        });
 
-    app.configure(function () {
-      app.use(express.bodyParser());
-    });
-    app.post('/api/test/:username', function (req, res) {
-      api.requireParams(req, res, ['username', 'password', 'token'], function (err) {
-        api.ok(req, res, { message: 'ok' });
-      });
-    });
-
-    request(app)
-      .post('/api/test/hercules?token=0')
-      .send({password: ''})
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .expect(JSON.stringify(data), done);
-  });
-
-  it('should respond with 400 status when POST request data is malformed', function (done) {
-    var app = express()
-      , expected = { message: 'Bad Request', errors: ['Missing required parameter: password'] };
-
-    app.configure(function () {
-      app.use(express.bodyParser());
-    });
-    app.post('/api/test', function (req, res) {
-      api.requireParams(req, res, ['username', 'password'], function (err) {
-        api.ok(req, res, { message: 'ok' });
-      });
+        request(app)
+            .post('/api/test/hercules?token=0')
+            .send({
+                password: ''
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(JSON.stringify(data), done);
     });
 
-    request(app)
-      .post('/api/test')
-      .send({ username: 'hercules' })
-      .expect('Content-Type', /json/)
-      .expect(400)
-      .expect(JSON.stringify(expected), done);
-  });
+    it('should respond with 400 status when POST request data is malformed', function(done) {
+        var app = express(),
+            expected = {
+                message: 'Bad Request',
+                errors: ['Missing required parameter: password']
+            };
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        app.post('/api/test', function(req, res) {
+            api.requireParams(req, res, ['username', 'password'], function(err) {
+                api.ok(req, res, {
+                    message: 'ok'
+                });
+            });
+        });
 
-  it('should respond with 400 status when GET request data is malformed', function (done) {
-    var app = express()
-      , expected = { message: 'Bad Request', errors: ['Missing required parameter: password'] };
-
-    app.get('/api/test', function (req, res) {
-      api.requireParams(req, res, ['username', 'password'], function (err) {
-        api.ok(req, res, { message: 'ok' });
-      });
+        request(app)
+            .post('/api/test')
+            .send({
+                username: 'hercules'
+            })
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .expect(JSON.stringify(expected), done);
     });
 
-    request(app)
-      .get('/api/test?username=hercules')
-      .expect('Content-Type', /json/)
-      .expect(400)
-      .expect(JSON.stringify(expected), done);
-  });
+    it('should respond with 400 status when GET request data is malformed', function(done) {
+        var app = express(),
+            expected = {
+                message: 'Bad Request',
+                errors: ['Missing required parameter: password']
+            };
 
-  it('should respond with 400 status when required request headers are missing', function (done) {
-    var app = express()
-      , expected = { message: 'Bad Request', errors: ['Missing required header parameter: X-Auth-Token'] };
+        app.get('/api/test', function(req, res) {
+            api.requireParams(req, res, ['username', 'password'], function(err) {
+                api.ok(req, res, {
+                    message: 'ok'
+                });
+            });
+        });
 
-    app.get('/api/test', function (req, res) {
-      api.requireHeaders(req, res, ['X-Auth-Token'], function (err) {
-        api.ok(req, res, { message: 'ok' });
-      });
+        request(app)
+            .get('/api/test?username=hercules')
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .expect(JSON.stringify(expected), done);
     });
 
-    request(app)
-      .get('/api/test')
-      .expect('Content-Type', /json/)
-      .expect(400)
-      .expect(JSON.stringify(expected), done);
-  });
+    it('should respond with 400 status when required request headers are missing', function(done) {
+        var app = express(),
+            expected = {
+                message: 'Bad Request',
+                errors: ['Missing required header parameter: X-Auth-Token']
+            };
 
-  it('should respond with 401 status when the client is unauthorized', function (done) {
-    var app = express()
-      , expected = { message: 'Unauthorized' };
+        app.get('/api/test', function(req, res) {
+            api.requireHeaders(req, res, ['X-Auth-Token'], function(err) {
+                api.ok(req, res, {
+                    message: 'ok'
+                });
+            });
+        });
 
-    app.get('/api/test', function (req, res) {
-      api.unauthorized(req, res);
+        request(app)
+            .get('/api/test')
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .expect(JSON.stringify(expected), done);
     });
 
-    request(app)
-      .get('/api/test')
-      .expect('Content-Type', /json/)
-      .expect(401)
-      .expect(JSON.stringify(expected), done);
-  });
+    it('should respond with 401 status when the client is unauthorized', function(done) {
+        var app = express(),
+            expected = {
+                message: 'Unauthorized'
+            };
 
-  it('should respond with 404 status when the requested resource does not exist', function (done) {
-    var app = express()
-      , expected = { message: 'Not Found' };
+        app.get('/api/test', function(req, res) {
+            api.unauthorized(req, res);
+        });
 
-    app.get('/api/test', function (req, res) {
-      api.notFound(req, res);
+        request(app)
+            .get('/api/test')
+            .expect('Content-Type', /json/)
+            .expect(401)
+            .expect(JSON.stringify(expected), done);
     });
 
-    request(app)
-      .get('/api/test')
-      .expect('Content-Type', /json/)
-      .expect(404)
-      .expect(JSON.stringify(expected), done);
-  });
+    it('should respond with 403 status when the client is forbidden', function(done) {
+        var app = express(),
+            expected = {
+                message: 'Forbidden'
+            };
 
-  it('should respond with 405 status when an unsupported API action is invoked', function (done) {
-    var app = express()
-      , expected = { message: 'Unsupported Action' };
+        app.get('/api/test', function(req, res) {
+            api.forbidden(req, res);
+        });
 
-    app.get('*', function (req, res) {
-      api.unsupportedAction(req, res);
+        request(app)
+            .get('/api/test')
+            .expect('Content-Type', /json/)
+            .expect(403)
+            .expect(JSON.stringify(expected), done);
     });
 
-    request(app)
-      .get('/api/nonexistent')
-      .expect('Content-Type', /json/)
-      .expect(405)
-      .expect(JSON.stringify(expected), done);
-  });
+    it('should respond with 404 status when the requested resource does not exist', function(done) {
+        var app = express(),
+            expected = {
+                message: 'Not Found'
+            };
 
-  it('should respond with 422 status when validation fails', function (done) {
-    var app = express()
-      , expected = { message: 'Validation Failed', errors: ['Username is already taken.'] };
+        app.get('/api/test', function(req, res) {
+            api.notFound(req, res);
+        });
 
-    app.configure(function () {
-      app.use(express.bodyParser());
-    });
-    app.post('/api/test', function (req, res) {
-      if (req.body.username === 'hercules')
-        api.invalid(req, res, 'Username is already taken.');
+        request(app)
+            .get('/api/test')
+            .expect('Content-Type', /json/)
+            .expect(404)
+            .expect(JSON.stringify(expected), done);
     });
 
-    request(app)
-      .post('/api/test')
-      .send({ username: 'hercules' })
-      .expect('Content-Type', /json/)
-      .expect(422)
-      .expect(JSON.stringify(expected), done);
-  });
+    it('should respond with 405 status when an unsupported API action is invoked', function(done) {
+        var app = express(),
+            expected = {
+                message: 'Unsupported Action'
+            };
 
-  it('should respond with 500 status for internal server errors', function (done) {
-    var app = express()
-      , expected = {
-          message: 'Internal Server Error',
-          error: {
-            message: 'Database error',
-            stacktrace: '...'
-          }
-        };
+        app.get('*', function(req, res) {
+            api.unsupportedAction(req, res);
+        });
 
-    app.get('/api/test', function (req, res) {
-      var err = new Error('Database error');
-      err.stack = '...';
-      api.serverError(req, res, err);
+        request(app)
+            .get('/api/nonexistent')
+            .expect('Content-Type', /json/)
+            .expect(405)
+            .expect(JSON.stringify(expected), done);
     });
 
-    request(app)
-      .get('/api/test')
-      .expect('Content-Type', /json/)
-      .expect(500)
-      .expect(JSON.stringify(expected), done);
-  });
+    it('should respond with 422 status when validation fails', function(done) {
+        var app = express(),
+            expected = {
+                message: 'Validation Failed',
+                errors: ['Username is already taken.']
+            };
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+
+        app.post('/api/test', function(req, res) {
+            if (req.body.username === 'hercules')
+                api.invalid(req, res, 'Username is already taken.');
+        });
+
+        request(app)
+            .post('/api/test')
+            .send({
+                username: 'hercules'
+            })
+            .expect('Content-Type', /json/)
+            .expect(422)
+            .expect(JSON.stringify(expected), done);
+    });
+
+    it('should respond with 500 status for internal server errors', function(done) {
+        var app = express(),
+            expected = {
+                message: 'Internal Server Error',
+                error: {
+                    message: 'Database error',
+                    stacktrace: '...'
+                }
+            };
+
+        app.get('/api/test', function(req, res) {
+            var err = new Error('Database error');
+            err.stack = '...';
+            api.serverError(req, res, err);
+        });
+
+        request(app)
+            .get('/api/test')
+            .expect('Content-Type', /json/)
+            .expect(500)
+            .expect(JSON.stringify(expected), done);
+    });
 });
